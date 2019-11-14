332298baef18b68fcb4ec764e79c3108d61f284d
augmity <augmity@augmity.com>
2019-11-13 20:53:04 -0800

    fix: for heroku 400 error

--- server.js
+++ server.js
@@ -60,7 +60,11 @@ const resolvers = {
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true
+});
 
 
 // The `listen` method launches a web server.
