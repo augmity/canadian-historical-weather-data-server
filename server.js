fb5b681782cf9f7a7df9caab8888689a1ed41f9e
augmity <augmity@augmity.com>
2019-11-13 20:44:08 -0800

    fix: apollo-server to use a port defined by Heroku at runtime

--- server.js
+++ server.js
@@ -57,11 +57,15 @@ const resolvers = {
   },
 };
 
+
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers });
 
+
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
+server
+  .listen({ port: process.env.PORT || 4000 })
+  .then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+  });
